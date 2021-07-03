aa233a2ecab89f60e042e8dc89d332026fb1749b
もっちゃん <s7056i3056@gmail.com>
2021-07-04 07:27:00 +0900

    スペース修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, only: [:/] #あとでコントローラーに移す？
+  before_action :authenticate_user!, only: [:/]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :email, :icon_img])
-    devise_parameter_sanitizer.permit(:sign_in, keys:[:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :icon_img])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
   end
 end
