d519e2ee016ead8175363a0610b972b2357ddd98
もっちゃん <s7056i3056@gmail.com>
2021-07-04 05:57:50 +0900

    fontawesomeの追加設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "./style.scss";
+import "../stylesheets/application.scss"
+import '@fortawesome/fontawesome-free/js/all'
 
 Rails.start()
 Turbolinks.start()
