26aaabf474db9644c755602e9596e555948f4c60
もっちゃん <s7056i3056@gmail.com>
2021-07-05 15:26:45 +0900

    profileの追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :login_user, except: [:profile_update]
+
+  def profile
+  end
+
+  def profile_update
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      redirect_to users_profile_path, notice: "プロフィールを更新しました"
+    else
+      render :profile
+    end
+  end
+
+
+  private
+
+  def login_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:new_icon_img, :name, :email, :description, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
   root 'home#top'
   devise_for :users
+  resources :fields do
+    get "search", on: :collection
+  end
+  namespace :users do
+    get 'profile'
+    patch 'profile_update'
+  end
 end
