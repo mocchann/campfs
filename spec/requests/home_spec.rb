21fa7f0957997c6346765541decf897e06120ac9
もっちゃん <s7056i3056@gmail.com>
2021-07-05 15:29:56 +0900

    specファイル追加

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe "Homes", type: :request do
   describe "GET /top" do
     it "returns http success" do
-      get "/"
+      get "/home/top"
       expect(response).to have_http_status(:success)
     end
   end
+
 end
