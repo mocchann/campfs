67faec99ad2cdaa7515ad933260fee7fe00f80ad
もっちゃん <s7056i3056@gmail.com>
2021-07-08 22:29:00 +0900

    jQueryの追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "../stylesheets/application.scss"
+import "../stylesheet/application.scss"
 import '@fortawesome/fontawesome-free/js/all'
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
 require("bootstrap");
+require('jquery')
