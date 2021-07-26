82ac7a00d14a8b9aad4d733ed713aedb83850abc
もっちゃん <s7056i3056@gmail.com>
2021-07-26 16:46:02 +0900

    ゲストユーザーログインの機能修正(削除不可に設定)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,4 @@
 class HomeController < ApplicationController
   def top
   end
-
-  # ゲストユーザーログイン
-  def guest_sign_in
-    user = User.find_or_create_by!(email: 'guest@example.com') do |gest|
-      gest.password = SecureRandom.urlsafe_base64
-    end
-    sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーさん、こんにちは！'
-  end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :ensure_normal_user, only: :destroy
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -59,4 +60,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  def ensure_normal_user
+    if resource.email == 'guest@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーさんは、削除できない設定になっています。ログアウトをお願いします。'
+    end
+  end
 end
