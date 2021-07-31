81e5c85136dd0cb014d00d566bb633b47075672d
もっちゃん <s7056i3056@gmail.com>
2021-07-31 21:19:41 +0900

    画像アップロード設定

--- app/models/field.rb
+++ app/models/field.rb
@@ -2,4 +2,9 @@ class Field < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :users, through: :reviews
   has_many :bookmarks, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :address, presence: true, length: { maximum: 100 }
+  validates :place_id, uniqueness: true
+  has_one_attached :image
 end
