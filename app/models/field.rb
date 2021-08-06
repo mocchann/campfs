c61b4b24d9bdae60f0bbf33f5b860ee3861de579
もっちゃん <s7056i3056@gmail.com>
2021-08-06 16:06:27 +0900

    pagenation表示の修正

--- app/models/field.rb
+++ app/models/field.rb
@@ -2,7 +2,7 @@ class Field < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :users, through: :reviews
   has_many :bookmarks, dependent: :destroy
-
+  paginates_per 9
   validates :name, presence: true, length: { maximum: 50 }
   validates :address, presence: true, length: { maximum: 100 }
   validates :place_id, uniqueness: true
