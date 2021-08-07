90643fb0887b06242dc483782321fd7aa42fc2a4
もっちゃん <s7056i3056@gmail.com>
2021-08-07 12:51:41 +0900

    本番環境でのパスワード再設定機能の追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -130,6 +130,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  host = 'to-camp.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { :host => 'to-camp.herokuapp.com' }
 end
