0008534860772a77cc32b0297306d68acf22ed9f
もっちゃん <s7056i3056@gmail.com>
2021-08-19 17:03:46 +0900

    mailgun smtp設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -131,4 +131,17 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   GA.tracker = "UA-174174667-2"
+
+  config.action_mailer.raise_delivery_errors = true
+  ActionMailer::Base.delivery_method = :smtp
+  host = 'to-camp.heroku.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => ,host
+    :authentication => :plain,
+  }
 end
