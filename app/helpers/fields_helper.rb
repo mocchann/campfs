297b6bf0dc000bae8b1d63f767d0f43d42067988
もっちゃん <s7056i3056@gmail.com>
2021-08-20 17:00:28 +0900

    件数を表示するhelperの設定

--- app/helpers/fields_helper.rb
+++ app/helpers/fields_helper.rb
@@ -7,4 +7,16 @@ module FieldsHelper
     end
     return rate.round(2)
   end
+
+  def fields_current_number(current_page, size)
+    if size > 8
+      first = 1
+      last  = size
+      last *= current_page
+      first = last -8
+      return "#{first} ~ #{last}"
+    else
+      return "1 ~ #{size}"
+    end
+  end
 end
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,13 @@
 module ReviewsHelper
+  def reviews_current_number(current_page, size)
+    if size > 2
+      first = 1
+      last  = size
+      last *= current_page
+      first = last -2
+      return "#{first} ~ #{last}"
+    else
+      return "1 ~ #{size}"
+    end
+  end
 end
