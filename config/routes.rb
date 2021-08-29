1c474b70c5a8d813f40e17cdc7c53dd9fd949614
もっちゃん <s7056i3056@gmail.com>
2021-08-29 22:07:08 +0900

    不要なコメントの削除

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     passwords: 'users/passwords'
   }
-  # ゲストユーザーログイン
+
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
