06b721fd9543b7d17c5d53f5c5bb02e3386f4984
もっちゃん <s7056i3056@gmail.com>
2021-09-09 22:59:45 +0900

    returnの削除

--- app/helpers/fields_helper.rb
+++ app/helpers/fields_helper.rb
@@ -5,18 +5,18 @@ module FieldsHelper
       reviews.each { |review| rate += review.rate.to_f }
       rate /= reviews.count
     end
-    return rate.round(2)
+    rate.round(2)
   end
-#TODO returnの削除
+
   def fields_current_number(current_page, size)
     if size > 8
       first = 1
       last  = size
       last *= current_page
       first = last -8
-      return "#{first} ~ #{last}"
+      "#{first} ~ #{last}"
     else
-      return "1 ~ #{size}"
+      "1 ~ #{size}"
     end
   end
 end
