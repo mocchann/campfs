ba3a338132bbc20ed911274979f5267967f2d512
もっちゃん <s7056i3056@gmail.com>
2026-02-14 21:46:52 +0900

    fix: lint

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   BASE_TITLE = "TO_CAMP".freeze
 
-  def full_title(page_title = '')
+  def full_title(page_title: '')
     if page_title.empty?
       BASE_TITLE
     else
